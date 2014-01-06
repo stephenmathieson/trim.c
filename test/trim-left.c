6eff8ae330a56d2fe30d52e8656305493c106d54
Stephen Mathieson <smath23@gmail.com>
2014-01-05 22:28:24 -0500

    refactor

--- test/trim-left.c
+++ test/trim-left.c
@@ -4,13 +4,13 @@
 #include "trim.h"
 #include "describe/describe.h"
 
-
 describe("trim_left", {
   it("should remove leading whitespace", {
     char str[] = "\t\n hello";
     assert_str_equal("hello", trim_left(str));
     assert_str_equal("hello", str);
   });
+
   it("should not remove trailing whitespace", {
     char str[] = "hello \t\n";
     assert_str_equal("hello \t\n", trim_left(str));
--- test/trim-right.c
+++ test/trim-right.c
@@ -4,13 +4,13 @@
 #include "trim.h"
 #include "describe/describe.h"
 
-
 describe("trim_right", {
   it("should not remove leading whitespace", {
     char str[] = "\t\n hello";
     assert_str_equal("\t\n hello", trim_right(str));
     assert_str_equal("\t\n hello", str);
   });
+
   it("should remove trailing whitespace", {
     char str[] = "hello \t\n";
     assert_str_equal("hello", trim_right(str));
